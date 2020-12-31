4954c73ab3293036ad4b1247dbc2558c338f229b
artush-alexanyan <artush.alexanyan1989@gmail.com>
2020-12-31 05:50:20 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <fieldImage />
+  </div>
+</template>
+
+<script>
+import fieldImage from './components/fieldImage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    fieldImage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/fieldImage.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="image-field">
+        <div class="container">
+            <div class="h4 text-center text-primary mt-5">
+                Image Field Component
+            </div>
+            <div class="row mt-5">
+                <div class="col col-6">
+                    <label for="">
+                            Change image 
+                            <strong>src</strong>
+                    </label>
+                    <p class="text-center text-secondary">
+                        Available src's 
+                        <strong>
+                            (vue.png, angular.jpeg, react.png)
+                        </strong>
+                    </p>
+                    <input type="text" 
+                           class="form-control"
+                           v-model="image"
+                        >
+                </div>
+                <div class="col col-4">
+                    <div class="card">
+                        <input type="image" 
+                               :src='require("../assets/" + validimages)'
+                            >
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'fieldImage',
+    data: () => ({
+        defaultImage: 'undefinde.png',
+        image: '',
+        images: ['vue.png', 'angular.jpeg', 'react.png']
+    }),
+    computed: {
+        validimages () {
+            return this.images.includes(this.image) ? this.image : this.defaultImage
+            }     
+        }
+}
+</script>
+
